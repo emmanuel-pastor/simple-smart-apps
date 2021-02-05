d6c8f7821d56daf484c114bdcc51a1241c3d510f
Emmanuel Pastor <paros7191@gmail.com>
2021-02-06 00:28:22 +0100

    Add Rating item
    Shows app rating and review count.

--- src/components/app-page/AppPage.jsx
+++ src/components/app-page/AppPage.jsx
@@ -4,6 +4,7 @@ import {PageState} from "../shared/PageState";
 import Headline from "./headline/Headline";
 import style from "./AppPage.module.css";
 import DownloadButtons from "./download-buttons/DownloadButtons";
+import Rating from "./rating/Rating";
 
 const AppPage = () => {
     const {pageState, appInfo} = useContext(AppPageContext);
@@ -12,9 +13,13 @@ const AppPage = () => {
         case PageState.READY :
             return (
                 <div className={style.appPage__mainWrapper}>
-                    <Headline app_icon={appInfo.images.app_icon_path} app_title={appInfo.name}
-                              install_count={appInfo.stats.install_count}/>
-                    <DownloadButtons playLink={appInfo.download_links.play_store} apkLink={appInfo.download_links.apk}/>
+                    <div className={style.appPage__topSection}>
+                        <Headline app_icon={appInfo.images.app_icon_path} app_title={appInfo.name}
+                                  install_count={appInfo.stats.install_count}/>
+                        <Rating rating={appInfo.stats.rating} review_count={appInfo.stats.review_count}/>
+                        <DownloadButtons playLink={appInfo.download_links.play_store}
+                                         apkLink={appInfo.download_links.apk}/>
+                    </div>
                 </div>
             )
         default:
---
+++ src/components/app-page/rating/Rating.jsx
@@ -0,0 +1,48 @@
+import PropTypes from "prop-types";
+import {FormattedMessage} from "react-intl";
+import style from "./Rating.module.css";
+import PersonIcon from "../../../assets/images/shared/person.svg";
+import RatingStar from "../../../assets/images/shared/rating_star.svg";
+import RatingStarGray from "../../../assets/images/shared/rating_star-gray.svg";
+
+const Rating = ({rating, review_count}) => {
+    const stars = []
+    for (let i = 0; i < 5; i++) {
+        if (i < Math.ceil(rating).toFixed(0) - 1) {
+            stars.push(<img key={i} src={RatingStar} alt={'Star icon'}/>)
+        } else if (i === Math.ceil(rating).toFixed(0) - 1) {
+            stars.push(
+                <div key={i} style={{backgroundImage: `url(${RatingStarGray})`}}>
+                    <div style={{
+                        backgroundImage: `url(${RatingStar})`,
+                        width: `calc(${rating % 1}*(0.572vw + 13px))`
+                    }}/>
+                </div>
+            )
+        } else {
+            stars.push(<img key={i} src={RatingStarGray} alt={'Star icon'}/>)
+        }
+    }
+
+    return (
+        <div className={style.ratingWrapper}>
+            <h2>{rating ? rating : '-'}</h2>
+            {<div className={style.ratingWrapper__stars}>{stars}</div>}
+            <p>
+                <img src={PersonIcon} alt={'Person icon'}/>
+                <FormattedMessage
+                    id="review_count"
+                    defaultMessage="{count, plural, =0 {No reviews} one {# review} other {# reviews}}"
+                    values={{count: review_count}}
+                />
+            </p>
+        </div>
+    )
+}
+
+Rating.propTypes = {
+    rating: PropTypes.number,
+    review_count: PropTypes.number.isRequired
+}
+
+export default Rating;
