5732a1a98f130aeeb6f405316f5da7fcc921487b
Emmanuel Pastor <paros7191@gmail.com>
2021-02-10 11:45:50 +0100

    Update FR Battery Temperature screenshots

--- src/assets/images/fr/battery-temp/BatteryTempImagesFR.js
+++ src/assets/images/fr/battery-temp/BatteryTempImagesFR.js
@@ -5,9 +5,10 @@ export const BatteryTempImagesFR = {
     app_icon_path: Icon,
     main_screenshot_path: MainScreen,
     screenshot_paths: [
-        "https://play-lh.googleusercontent.com/bNVJU1BGvVpTdSZACE1Be4jaNJiJK-8phiCXvyaOU1_rzu1L5_nKejNAqxUnPDJvFwTB=w1536-h722-rw",
-        "https://play-lh.googleusercontent.com/lLcnG2MY2ymxsLcsTAj6jcRICj2TpnrISXNo8ZDEmdL_v0NkrFQrabsKHmksUcSSDaPD=w1536-h722",
-        "https://play-lh.googleusercontent.com/mhLQxffb43JLXyOkerz1qqcNOU06XLc3BjPpQWEG5rG3oLDLrBdz6XhSAaKmlHaADQ=w1536-h722",
-        "https://play-lh.googleusercontent.com/lINR8euMzHpWwuxphQyFcOFIT7H5oMmmy-afUlz-4lG_4SgeernNWNFDg6Ch2cDTvA=w1536-h722",
+        "https://play-lh.googleusercontent.com/QUac4bnArYhaJFz1xET7P9bnjIXHrZu75U7AQfQZfYcs21Tkbv1navxtmza864dw4CCd=w1536-h722",
+        "https://play-lh.googleusercontent.com/9FI9UXcOmO9tweRIxVWlujEc8p4DFGMC5NNkVo4gdCuP1x3bdGDUtazkGZOMd9j9UoI=w1536-h722",
+        "https://play-lh.googleusercontent.com/pOd4UIpFfENSUfwVje_2kcWgLefUFjhGXaPqn9WYOd6-WozJ8oiHMCCzDVIfuAPjXQ=w1536-h722",
+        "https://play-lh.googleusercontent.com/D3vV3PbjNZaiM6hEjer5WEzbxtZo5n-4uRshfFcK4WyUiyie8c4oU_fckho55MohxxM=w1536-h722",
+        "https://play-lh.googleusercontent.com/iBKVnMIhDdADZpmAhrADJj3iw3eTZxJjfjqashs6BfC291Wfppq0N6L53jBiFua2ng=w1536-h722"
     ]
 }
