1a98252cf8f20d8b03e3ae20fc9edf0bc810f74e
Emmanuel Pastor <ommanu13@hotmail.fr>
2022-04-26 02:14:58 +0100

    Update Battery Temp app stats

--- src/data/LocalDataStore.tsx
+++ src/data/LocalDataStore.tsx
@@ -36,11 +36,11 @@ Keeping your battery temperature at a reasonable level expands the lifetime of y
         images: batteryTempImages,
         stats: {
             rating: 4.6,
-            review_count: 6847,
+            review_count: 6951,
             update_date: '2020-02-18',
             release_date: '2018-01-27',
             apk_size: 2.9,
-            install_count: 593335,
+            install_count: 601721,
             current_version: '1.9.0',
             min_android_version: 5.0
         }
