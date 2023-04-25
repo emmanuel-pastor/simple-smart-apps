dd65352a8325cbbb61e36dd03b37f0676c52c234
Emmanuel Pastor <ommanu13@hotmail.fr>
2023-04-25 21:53:46 +0200

    Update Battery Temp app stats

--- src/data/LocalDataStore.tsx
+++ src/data/LocalDataStore.tsx
@@ -33,11 +33,11 @@ export default function createLocalDataStore(locale: string, intl: IntlShape) {
         images: batteryTempImages,
         stats: {
             rating: 4.5,
-            review_count: 9293,
+            review_count: 9337,
             update_date: '2022-10-29',
             release_date: '2018-01-27',
             apk_size: 3.78,
-            install_count: 878620,
+            install_count: 886016,
             current_version: '1.9.7',
             min_android_version: 5.0
         }
